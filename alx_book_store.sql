8ec9ac1979a29b151e4a2ea422b8d3afae295167
Yilikal250 <teshomeyilikal250@gmail.com>
2024-07-25 23:20:31 +0200

    DATABASE AND TABLES CREATED DATABASE

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,21 +1,21 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store
 
 CREATE TABLE Books (
-    book_id (Primary Key),
+    book_id PRIMARY KEY,
     title VARCHAR(130),
     author_id (Foreign Key referencing Authors table),
     price DOUBLE,
     publication_date DATE
 );
 CREATE TABLE Authors (
-    author_id (Primary Key),
+    author_id PRIMARY KEY,
     author_name VARCHAR(215),
     author_id (Foreign Key referencing Authors table),
     price DOUBLE,
     publication_date DATE
 );
 CREATE TABLE Customers (
-    customer_id (Primary Key),
+    customer_id PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT,
@@ -23,14 +23,14 @@ CREATE TABLE Customers (
 );
 
 CREATE TABLE Orders (
-    order_id (Primary Key),
+    order_id PRIMARY KEY,
     customer_id (Foreign Key referencing Customers table),
     email VARCHAR(215),
     order_date DATE
 );
 
 CREATE TABLE Order_Details (
-    orderdetailid (Primary Key),
+    orderdetailid PRIMARY KEY,
     order_id (Foreign Key referencing Orders table),
     book_id (Foreign Key referencing Books table),
     quantity DOUBLE
