d3df1f8878d3d28b16b15353b877815ce4e99ed5
Yilikal250 <teshomeyilikal250@gmail.com>
2024-07-25 23:28:07 +0200

    DATABASE AND TABLES CREATED DATABASE

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -31,8 +31,8 @@ CREATE TABLE Orders (
 
 CREATE TABLE Order_Details (
     orderdetailid PRIMARY KEY,
-    order_id (FOREIGN KEY (order_id) REFERENCES Orders(order_id)),
-    book_id (FOREIGN KEY (book_id) REFERENCES Books(book_id)),
+    order_id INT (FOREIGN KEY (order_id) REFERENCES Orders(order_id)),
+    book_id INT(FOREIGN KEY (book_id) REFERENCES Books(book_id)),
     quantity DOUBLE
 );
 
